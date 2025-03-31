f1b6a8279660207275fe1a873fd3cfd72fea6c94
RT-art <mailnosekai@gmail.com>
2025-03-31 19:52:26 +0900

    ディレクトリ構造をベストプラクティスに沿った構造に。DRYで再利用可能な構造に進化

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,17 +1,17 @@
 module "network" {
-  source                   = "./modules/network"
+  source                   = "../../modules/network"
   module_vpc_cidr_block    = var.root_vpc_cidr_block
   module_subnet_cidr_block = var.root_subnet_cidr_block
   module_availability_zone = var.root_availability_zone
 }
 
 module "security_group" {
-  source = "./modules/security_group"
+  source        = "../../modules/security_group"
   module_vpc_id = module.network.vpc_id
 }
 
 module "ec2_instance" {
-  source                   = "./modules/ec2_instance"
+  source                   = "../../modules/ec2_instance"
   module_instance_type     = var.root_instance_type
   module_ami_name_filter   = var.root_ami_name_filter
   module_instance_key_name = var.root_instance_key_name
