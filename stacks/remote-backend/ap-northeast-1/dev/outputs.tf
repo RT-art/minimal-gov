715a5e18162a3cb12fbc4ed4eaa46d0f4452bac6
RT <mailnosekai@gmail.com>
2025-08-16 21:24:49 +0900

    outputsの出力を調整。

--- stacks/remote-backend/ap-northeast-1/dev/outputs.tf
+++ stacks/remote-backend/ap-northeast-1/dev/outputs.tf
@@ -1,3 +1,6 @@
 output "tfstate_bucket_name" {
   value = module.terraform_remote_backend.state_bucket_name
 }
+output "tfstate_bucket_arn" {
+  value = module.terraform_remote_backend.state_bucket_arn
+}
--- stacks/remote-backend/ap-northeast-1/dev/variable.tf
+++ stacks/remote-backend/ap-northeast-1/dev/variable.tf
@@ -3,18 +3,27 @@
 ########################################
 
 variable "env" {
-  description = "The name of the environment."
-  type        = string
+  type = string
+  validation {
+    condition     = can(regex("^(dev|stg|prod|sandbox)$", var.env))
+    error_message = "env は dev|stg|prod|sandbox のいずれか。"
+  }
 }
 
 variable "app_name" {
-  description = "The name of the application."
-  type        = string
+  type = string
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9-_]{3,32}$", var.app_name))
+    error_message = "app_name は 3–32 文字の英数/ハイフン/アンダースコア。"
+  }
 }
 
 variable "region" {
-  description = "AWS region"
-  type        = string
+  type = string
+  validation {
+    condition     = can(regex("^[a-z]{2}-[a-z]+-\\d$", var.region))
+    error_message = "region の形式が不正。例: ap-northeast-1"
+  }
 }
 
 ########################################
@@ -24,14 +33,24 @@ variable "region" {
 variable "versioning_enabled" {
   description = "Enable versioning. Once you version-enable a bucket, it can never return to an unversioned state."
   type        = bool
+  default     = true
 }
 
 variable "server_side_encryption_configuration" {
   description = "Map containing server-side encryption configuration."
   type        = any
+  default     = {
+    rule = [{
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+      bucket_key_enabled = true
+    }]
+  }
 }
 
 variable "control_object_ownership" {
   description = "Whether to manage S3 Bucket Ownership Controls on this bucket."
   type        = bool
+  default     = true
 }
