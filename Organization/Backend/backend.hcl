3f42ce5ff4835eb0d17d9b7ecf6a109590e2ea6c
rt-art <mailnosekai@gmail.com>
2025-08-27 08:27:12 +0900

    リモートバックエンド＆AWS Organizationの最低限の構成を追加

---
+++ Organization/Backend/backend.hcl
@@ -0,0 +1,5 @@
+bucket         = "secure-backend-s3-tfstate-ap-northeast-1-911167886978"
+key            = "states/dev/ap-northeast-1/terraform.tfstate"
+region         = "ap-northeast-1"
+encrypt        = true
+use_lockfile   = true
---
+++ Organization/Backend/main.tf
@@ -0,0 +1,12 @@
+module "terraform_remote_backend" {
+  source             = "../../modules/backend/"
+  env                = var.env
+  app_name           = var.app_name
+  region             = var.region
+  tags               = var.tags
+  versioning_enabled = var.versioning_enabled
+  force_destroy      = var.force_destroy
+  lifecycle_days     = var.lifecycle_days
+
+  
+}
