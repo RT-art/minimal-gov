70b19912201a35b4ca9ba09f06bbe04bf60a1917
RT-art <mailnosekai@gmail.com>
2025-09-03 14:39:02 +0900

    TGWをRAM共有し、DevとNetworkのVPCを繋ぐ設定を追加

--- Accounts/Network/main.tf
+++ Accounts/Network/main.tf
@@ -1,19 +1,4 @@
-# minimal-gov/Accounts/Network/main.tf
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
-# --- VPC & サブネット ---------------------------------------------------
-
+# --- VPC ---------------------------------------------------------
 resource "aws_vpc" "main" {
   cidr_block           = "192.168.0.0/16"
   enable_dns_support   = true
@@ -35,8 +20,7 @@ resource "aws_subnet" "private_c" {
   tags              = { Name = "private-1c" }
 }
 
-# --- Transit Gateway ハブ ------------------------------------------------
-
+# --- Transit Gateway (TGW) --------------------------------------
 resource "aws_ec2_transit_gateway" "hub" {
   description                    = "central transit gateway"
   amazon_side_asn                = 64512
@@ -44,11 +28,10 @@ resource "aws_ec2_transit_gateway" "hub" {
   vpn_ecmp_support               = "enable"
   multicast_support              = "disable"
   auto_accept_shared_attachments = "enable"
-
-  tags = { Name = "tgw-hub" }
+  tags                           = { Name = "tgw-hub" }
 }
 
-# VPC を TGW にアタッチ (必要に応じて削除可)
+# Network VPC を TGW にアタッチ
 resource "aws_ec2_transit_gateway_vpc_attachment" "hub_vpc" {
   transit_gateway_id = aws_ec2_transit_gateway.hub.id
   vpc_id             = aws_vpc.main.id
@@ -59,9 +42,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "hub_vpc" {
   tags = { Name = "tgw-attachment" }
 }
 
-# --- RAM による TGW 共有 -------------------------------------------------
-
-# 組織情報を取得 (既存の AWS Organizations 前提)
+# --- TGW を AWS Organizations 全体へ共有 -------------------------
 data "aws_organizations_organization" "current" {}
 
 resource "aws_ram_resource_share" "tgw_share" {
@@ -69,13 +50,11 @@ resource "aws_ram_resource_share" "tgw_share" {
   allow_external_principals = false
 }
 
-# 共有対象: 組織全体
 resource "aws_ram_principal_association" "tgw_org" {
   resource_share_arn = aws_ram_resource_share.tgw_share.arn
   principal          = data.aws_organizations_organization.current.arn
 }
 
-# TGW を RAM リソース共有に紐付け
 resource "aws_ram_resource_association" "tgw_assoc" {
   resource_share_arn = aws_ram_resource_share.tgw_share.arn
   resource_arn       = aws_ec2_transit_gateway.hub.arn
