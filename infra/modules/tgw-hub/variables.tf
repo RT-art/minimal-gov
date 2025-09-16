8a6a543bc251e77d51e795e7dc7e85c2253f5a3b
RT-art <mailnosekai@gmail.com>
2025-09-16 15:20:20 +0900

    fix

--- infra/modules/tgw-hub/variables.tf
+++ infra/modules/tgw-hub/variables.tf
@@ -1,6 +1,20 @@
 ###############################################
 # Metadata
 ###############################################
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "app_name" {
+  type        = string
+  description = "Application name"
+}
+
+variable "env" {
+  type        = string
+  description = "Environment name (dev/stg/prod)"
+}
 
 variable "tags" {
   type    = map(string)
