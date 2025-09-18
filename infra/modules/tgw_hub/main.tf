a2c536bedbc70801187bc872636de8aa84ba0855
RT-art <mailnosekai@gmail.com>
2025-09-18 17:24:14 +0900

    fix

--- infra/modules/tgw_hub/main.tf
+++ infra/modules/tgw_hub/main.tf
@@ -34,8 +34,7 @@ resource "aws_ram_principal_association" "dev" {
   resource_share_arn = aws_ram_resource_share.this.arn
 }
 
-resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  subnet_ids         = [<subnet-id-1>, <subnet-id-2>]
-  transit_gateway_id = "tgw-0bcfdde6ac5e2575d"
-  vpc_id             = <dev-vpc-id>
+resource "aws_ram_resource_association" "tgw" {
+  resource_share_arn = aws_ram_resource_share.this.arn
+  resource_arn       = aws_ec2_transit_gateway.this.arn
 }
